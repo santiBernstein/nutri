d132151c405988236ee0746e41a767454a0ad356
santiBernstein <santiago.bernstein@gmail.com>
2022-05-05 14:44:25 -0300

    scroll js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,22 @@
-//show menu
-const showMenu = (toggleId,navId) => {
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+/*show menu*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show-menu')
-        })
-    }
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
 }
-showMenu('nav-toggle','nav-menu');
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 // remove menu mobile
 
@@ -17,6 +24,46 @@ const navLink = document.querySelectorAll('.nav--link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+          navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// scroll sections active link
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+                sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav--menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav--menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// change background header 
+
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+// show scroll top 
+
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
