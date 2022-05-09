2efe3f05056f5a302f9774164bdf5020bf60d969
santiBernstein <santiago.bernstein@gmail.com>
2022-05-08 21:51:11 -0300

    armado section home, cambios en nav menu, comienzo de armado section about

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,3 +67,6 @@ function scrollTop(){
     if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
+
+
+
